a52656bad74778d431211707827bd39892d1dc58
akcorp2003 <kuanga2016@ucla.edu>
2016-01-27 01:57:08 -0800

    Minor Corrections
    Edited .dtd file for header, some small changes in testing

--- src/Entry.java
+++ src/Entry.java
@@ -1,5 +1,7 @@
 import java.io.*;
 
+import org.w3c.dom.*;
+
 public class Entry {
 	
 	public static void main(String args[])
@@ -8,10 +10,32 @@ public class Entry {
 		//arg[0] the DTD document
 		//arg[i] where i != 0 , XML document
 		
-		InsertDTD DTDinserter = new InsertDTD(args[1], args[0]);
-		DTDinserter.add_dtd();
+		/*InsertDTD DTDinserter = new InsertDTD(args[1], args[0]);
+		DTDinserter.add_dtd();*/
 		
 		XMLBuilder b_xml = new XMLBuilder(args[1]);
 		b_xml.buildXML();
+		
+		//testing
+		System.out.println(b_xml.get_root().getNodeName());
+		
+		Document xml_doc = b_xml.get_document();
+		NodeList nList = xml_doc.getElementsByTagName("Item");
+		System.out.println("============================");
+		 
+		for (int i = 0; i < nList.getLength(); i++)
+		{
+		 Node node = nList.item(i);
+		 System.out.println("");    //Just a separator
+		 if (node.getNodeType() == Node.ELEMENT_NODE)
+		 {
+		    //Print each employee's detail
+		    Element eElement = (Element) node;
+		    System.out.println("Name : "    + eElement.getElementsByTagName("Name").item(0).getTextContent());
+		    System.out.println("Category : "  + eElement.getElementsByTagName("Category").item(0).getTextContent());
+		    System.out.println("Bids : "   + eElement.getElementsByTagName("Bids").item(0).getTextContent());
+		    System.out.println("Location : "    + eElement.getElementsByTagName("Location").item(0).getTextContent());
+		 }
+		}
 	}
 }
--- src/XMLBuilder.java
+++ src/XMLBuilder.java
@@ -7,6 +7,7 @@ import java.io.*;
 public class XMLBuilder {
 	
 	private String _xml_filename;
+	private Document _document;
 	
 	public XMLBuilder(String xmlfilename){
 		set_xml_filename(xmlfilename);
@@ -20,16 +21,26 @@ public class XMLBuilder {
 		this._xml_filename = _xml_filename;
 	}
 	
+	public Document get_document(){
+		return _document;
+	}
+	
 	public void buildXML() {
 		DocumentBuilderFactory b_factory = DocumentBuilderFactory.newInstance();
 		b_factory.setValidating(true); //validate the XML document
 		try {
 			DocumentBuilder builder = b_factory.newDocumentBuilder();
 			Document my_doc = builder.parse(new File(this.get_xml_filename()));
+			_document = my_doc;
 			my_doc.getDocumentElement().normalize(); //makes sure everything is kept neat and tidy
 		} catch (ParserConfigurationException | SAXException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public Element get_root(){
+		Element root = _document.getDocumentElement();
+		return root;
 	}
 }
